cea4bf465d7f068d5ee9ec9c73cf72f9364de0e6
Prashant Shetty <psshetty@gmail.com>
2017-01-02 21:41:20 -0600

    removed JSONRawValue

--- src/main/java/com/hc/chat/message/pojo/Link.java
+++ src/main/java/com/hc/chat/message/pojo/Link.java
@@ -1,13 +1,10 @@
 package com.hc.chat.message.pojo;
 
-import com.fasterxml.jackson.annotation.JsonRawValue;
-
 /**
  * Created by ps on 12/27/16.
  */
 public class Link {
   private String url;
-  //@JsonRawValue
   private String title;
 
   public Link() {}
